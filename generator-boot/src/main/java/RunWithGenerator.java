f8a670f520d4f021c64ba6e4bc2ccc92ab6279a8
thenx <admin@thenx.com.cn>
2019-12-21 22:43:02 +0800

    add: 添加 插件及开源协议

--- generator-boot/src/main/java/RunWithGenerator.java
+++ generator-boot/src/main/java/RunWithGenerator.java
@@ -1,3 +1,23 @@
+/*-
+ * <<
+ * thenx-generator
+ * >
+ * Copyright (C) 2019 thenx
+ * >
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * >>
+ */
+
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
@@ -23,7 +43,7 @@ public class RunWithGenerator {
 
     public void generator() throws Exception{
 
-        List<String> warnings = new ArrayList<>();
+        List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
         InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("generatorConfig.xml");
         ConfigurationParser cp = new ConfigurationParser(warnings);
