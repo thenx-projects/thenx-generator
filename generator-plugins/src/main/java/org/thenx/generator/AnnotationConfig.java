ccb3eca33ef06ef2e156d544672921ea4f44fe51
Minny Park <opensource@thenx.org>
2020-12-23 11:20:11 +0800

    fixed: 更新协议

--- generator-plugins/src/main/java/org/thenx/generator/AnnotationConfig.java
+++ generator-plugins/src/main/java/org/thenx/generator/AnnotationConfig.java
@@ -1,21 +1,19 @@
-/*-
- * <<
- * thenx-generator
- * >
- * Copyright (C) 2019 thenx
- * >
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * >>
+
+
+/*
+ *    Copyright [yyyy] [name of copyright owner]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package org.thenx.generator;
@@ -34,19 +32,8 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-/**
- * @author thenx
- * <p>
- * 自定义一个插件，用于第三方扩展
- */
 public class AnnotationConfig extends PluginAdapter {
 
-    /**
-     * 开启验证
-     *
-     * @param list
-     * @return null
-     */
     @Override
     public boolean validate(List<String> list) {
         return true;
--- generator-plugins/src/main/java/org/thenx/generator/GeneratorConfig.java
+++ generator-plugins/src/main/java/org/thenx/generator/GeneratorConfig.java
@@ -1,21 +1,19 @@
-/*-
- * <<
- * thenx-generator
- * >
- * Copyright (C) 2019 thenx
- * >
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * >>
+
+
+/*
+ *    Copyright [yyyy] [name of copyright owner]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package org.thenx.generator;
@@ -37,16 +35,8 @@ import java.util.Set;
 
 import static org.mybatis.generator.internal.util.StringUtility.isTrue;
 
-/**
- * @author thenx
- * <p>
- * 加强 MyBatis的逆向功能
- */
 public class GeneratorConfig implements CommentGenerator {
 
-    /**
-     * properties 配置文件
-     */
     private Properties properties;
 
     /**
